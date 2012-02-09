4895fda3863c15a502d999ff99bf1098a8883337
Sun Ning <classicning@gmail.com>
2012-02-09 21:36:09 +0800

    render background

--- heatcanvas.js
+++ heatcanvas.js
@@ -96,13 +96,12 @@ HeatCanvas.prototype._render = function(f_value_color){
     var ctx = this.canvas.getContext("2d");
     ctx.clearRect(0, 0, this.width, this.height);
     
-    // reader background as black
-    ctx.fillStyle = this.bgcolor || "rgb(0,0,0)";
-    ctx.fillRect(0, 0, this.width, this.height);
-
     var canvasData = ctx.createImageData(this.width, this.height);
     for (var i=0; i<canvasData.data.length; i+=4){
-        canvasData.data[i] = 0; // transparentize all pixels
+        canvasData.data[i] = this.bgcolor[0]; // r
+        canvasData.data[i+1] = this.bgcolor[1];
+        canvasData.data[i+2] = this.bgcolor[2];
+        canvasData.data[i+3] = this.bgcolor[3];
     }
     
     // maximum 
