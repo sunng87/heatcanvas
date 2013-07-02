719e10601c478db3b7e52eb657f8b086f3424d21
Sun Ning <classicning@gmail.com>
2013-07-02 22:14:01 +0800

    amd wrapper for heatcanvas and its leaflet plugin

--- heatcanvas-leaflet.js
+++ heatcanvas-leaflet.js
@@ -20,103 +20,114 @@
  * SOFTWARE.
  */
 
-L.TileLayer.HeatCanvas = L.Class.extend({
+(function (root, factory) {
+  if (typeof define === 'function' && define.amd) {
+    // AMD. Register as an anonymous module.
+    define(['heatcanvas', 'leaflet'], factory);
+  } else {
+    // Browser globals
+    L.TileLayer.HeatCanvas = factory(HeatCanvas, L);
+  }
+}(this, function (HeatCanvas, L) {
+  var HeatCanvasLeafLet = L.Class.extend({
 
     initialize: function(options, heatCanvasOptions){
-        this.heatCanvasOptions = heatCanvasOptions;
-        this.data= [];
-        this._onRenderingStart = null;
-        this._onRenderingEnd = null;
+      this.heatCanvasOptions = heatCanvasOptions;
+      this.data= [];
+      this._onRenderingStart = null;
+      this._onRenderingEnd = null;
     },
 
     onRenderingStart: function(cb){
-        this._onRenderingStart = cb;
+      this._onRenderingStart = cb;
     },
 
     onRenderingEnd: function(cb) {
-        this._onRenderingEnd = cb;
+      this._onRenderingEnd = cb;
     },
 
     onAdd: function(map) {
-        this.map = map;
-        this._initHeatCanvas(this.map, this.heatCanvasOptions);
-        map.on("moveend", this._redraw, this);
-        this._redraw();
+      this.map = map;
+      this._initHeatCanvas(this.map, this.heatCanvasOptions);
+      map.on("moveend", this._redraw, this);
+      this._redraw();
     },
 
     onRemove: function(map) {
-        map.getPanes().overlayPane.removeChild(this._div);
-        map.off("moveend", this._redraw, this);
+      map.getPanes().overlayPane.removeChild(this._div);
+      map.off("moveend", this._redraw, this);
     },
 
     _initHeatCanvas: function(map, options){
-        options = options || {};                        
-        this._step = options.step || 1;
-        this._degree = options.degree || HeatCanvas.LINEAR;
-        this._opacity = options.opacity || 0.6;
-        this._colorscheme = options.colorscheme || null;
+      options = options || {};                        
+      this._step = options.step || 1;
+      this._degree = options.degree || HeatCanvas.LINEAR;
+      this._opacity = options.opacity || 0.6;
+      this._colorscheme = options.colorscheme || null;
 
-        var container = L.DomUtil.create('div', 'leaflet-heatmap-container');
-        container.style.position = 'absolute';
-        container.style.width = this.map.getSize().x+"px";
-        container.style.height = this.map.getSize().y+"px";
+      var container = L.DomUtil.create('div', 'leaflet-heatmap-container');
+      container.style.position = 'absolute';
+      container.style.width = this.map.getSize().x+"px";
+      container.style.height = this.map.getSize().y+"px";
 
-        var canv = document.createElement("canvas");
-        canv.width = this.map.getSize().x;
-        canv.height = this.map.getSize().y;
-        canv.style.width = canv.width+"px";
-        canv.style.height = canv.height+"px";
-        canv.style.opacity = this._opacity;
-        container.appendChild(canv);
+      var canv = document.createElement("canvas");
+      canv.width = this.map.getSize().x;
+      canv.height = this.map.getSize().y;
+      canv.style.width = canv.width+"px";
+      canv.style.height = canv.height+"px";
+      canv.style.opacity = this._opacity;
+      container.appendChild(canv);
 
-        this.heatmap = new HeatCanvas(canv);
-        this.heatmap.onRenderingStart = this._onRenderingStart;
-        this.heatmap.onRenderingEnd = this._onRenderingEnd;
-        this._div = container;
-        this.map.getPanes().overlayPane.appendChild(this._div);
+      this.heatmap = new HeatCanvas(canv);
+      this.heatmap.onRenderingStart = this._onRenderingStart;
+      this.heatmap.onRenderingEnd = this._onRenderingEnd;
+      this._div = container;
+      this.map.getPanes().overlayPane.appendChild(this._div);
     },
 
     pushData: function(lat, lon, value) {
-        this.data.push({"lat":lat, "lon":lon, "v":value});
+      this.data.push({"lat":lat, "lon":lon, "v":value});
     },
     
     _resetCanvasPosition: function() {
-        var bounds = this.map.getBounds();
-        var topLeft = this.map.latLngToLayerPoint(bounds.getNorthWest());
-        L.DomUtil.setPosition(this._div, topLeft);
+      var bounds = this.map.getBounds();
+      var topLeft = this.map.latLngToLayerPoint(bounds.getNorthWest());
+      L.DomUtil.setPosition(this._div, topLeft);
     },
 
     _redraw: function() {
-        this._resetCanvasPosition();
-        this.heatmap.clear();
-        if (this.data.length > 0) {
-            for (var i=0, l=this.data.length; i<l; i++) {
-                var lonlat = new L.LatLng(this.data[i].lat, this.data[i].lon);
-                var localXY = this.map.latLngToLayerPoint(lonlat);
-                localXY = this.map.layerPointToContainerPoint(localXY);
-                this.heatmap.push(
-                        Math.floor(localXY.x), 
-                        Math.floor(localXY.y), 
-                        this.data[i].v);
-            }
-
-            this.heatmap.render(this._step, this._degree, this._colorscheme);
+      this._resetCanvasPosition();
+      this.heatmap.clear();
+      if (this.data.length > 0) {
+        for (var i=0, l=this.data.length; i<l; i++) {
+          var lonlat = new L.LatLng(this.data[i].lat, this.data[i].lon);
+          var localXY = this.map.latLngToLayerPoint(lonlat);
+          localXY = this.map.layerPointToContainerPoint(localXY);
+          this.heatmap.push(
+            Math.floor(localXY.x), 
+            Math.floor(localXY.y), 
+            this.data[i].v);
         }
-        return this;
+
+        this.heatmap.render(this._step, this._degree, this._colorscheme);
+      }
+      return this;
     },
 
     clear: function(){
-        this.heatmap.clear();
-		this.data = [];
+      this.heatmap.clear();
+      this.data = [];
     },
 
     redraw: function(){
-        this._redraw();
+      this._redraw();
     }
 
-});
+  });
 
-L.TileLayer.heatcanvas = function (options) {
-    return new L.TileLayer.HeatCanvas(options);
-};
-}
+  var heatcanvas = function (options) {
+    return new HeatCanvasLeaflet(options);
+  };
+  return heatcanvas;
+
+}));
