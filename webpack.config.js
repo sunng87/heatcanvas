cd6d0be9b0fd8b306c781ce830d74a9c4f4b1b6b
Josh Hansen <johansen@adobe.com>
2017-10-24 14:11:26 -0600

    Split the different variant builds into separate webpack outputs.

--- webpack.config.js
+++ webpack.config.js
@@ -1,12 +1,81 @@
 const path = require('path');
+const CopyWebpackPlugin = require('copy-webpack-plugin');
 
-module.exports = {
+module.exports = [{
     entry: {
         app: "./src/app.js",
     },
     output: {
         path: __dirname + "/dist/",
-        filename: "heatcanvas.js"
+        filename: "heatcanvas.js",
+        library: "HeatCanvas",
+        libraryExport: "default"
+    },
+    devtool: 'source-map',
+    plugins: [
+        new CopyWebpackPlugin([
+            {from:'src/heatcanvas-worker.js',to:'heatcanvas-worker.js'} 
+        ]),
+    ]
+},
+{
+    entry: {
+        app: "./src/heatcanvas-51map.js"
+    },
+    output: {
+        path: __dirname + "/dist/",
+        filename: "heatcanvas-51map.js",
+        library: "HeatCanvas51Layer",
+        libraryExport: "default"
     },
     devtool: 'source-map'
-};
+},
+{
+    entry: {
+        app: "./src/heatcanvas-baidumap.js"
+    },
+    output: {
+        path: __dirname + "/dist/",
+        filename: "heatcanvas-baidumap.js",
+        library: "HeatCanvasBaiduLayer",
+        libraryExport: "default"
+    },
+    devtool: 'source-map'
+},
+{
+    entry: {
+        app: "./src/heatcanvas-googlemaps.js"
+    },
+    output: {
+        path: __dirname + "/dist/",
+        filename: "heatcanvas-googlemaps.js",
+        library: "HeatCanvasOverlayView",
+        libraryExport: "default"
+    },
+    devtool: 'source-map'
+},
+{
+    entry: {
+        app: "./src/heatcanvas-leaflet.js"
+    },
+    output: {
+        path: __dirname + "/dist/",
+        filename: "heatcanvas-leaflet.js",
+        library: "HeatCanvasLeaflet",
+        libraryExport: "default"
+    },
+    devtool: 'source-map'
+},
+{
+    entry: {
+        app: "./src/heatcanvas-openlayers.js"
+    },
+    output: {
+        path: __dirname + "/dist/",
+        filename: "heatcanvas-openlayers.js",
+        library: "HeatCanvasOpenLayers",
+        libraryExport: "default"
+    },
+    devtool: 'source-map'
+}
+];
